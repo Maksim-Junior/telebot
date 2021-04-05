2866458b48d08e7d32576e67a7454c9fd931c09d
Maksim-Junior <72498963+Maksim-Junior@users.noreply.github.com>
2021-04-05 22:07:26 +0300

    edit Procfile

---
+++ src/runner.py
@@ -0,0 +1,13 @@
+import uvicorn
+from dotenv import load_dotenv
+
+load_dotenv()
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "asgi:app",
+        host="localhost",
+        log_level="debug",
+        port=8000,
+        reload=True,
+    )

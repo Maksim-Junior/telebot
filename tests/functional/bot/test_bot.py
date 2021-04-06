743b9852a8403705bb4efe32e7a16897d2d564c4
Maksim-Junior <72498963+Maksim-Junior@users.noreply.github.com>
2021-04-06 12:59:09 +0300

    add functional tests

---
+++ tests/functional/bot/test_bot.py
@@ -0,0 +1,28 @@
+import os
+
+import pytest
+from starlette import status
+from starlette.testclient import TestClient
+
+from asgi import app
+
+client = TestClient(app
+                    )
+
+
+@pytest.mark.functional
+def test_success_local():
+    response = client.get("/config/")
+    assert response.status_code == status.HTTP_200_OK
+    payload = response.json()
+    data = {"bot_token": os.getenv('BOT_TOKEN'), "pythonpath": os.getenv('PYTHONPATH')}
+    assert payload == data
+
+
+@pytest.mark.functional
+def test_success_heroku():
+    response = client.get("https://telebotek.herokuapp.com/config/")
+    assert response.status_code == status.HTTP_200_OK
+    payload = response.json()
+    data = {"bot_token": os.getenv('BOT_TOKEN'), "pythonpath": os.getenv('PYTHONPATH')}
+    assert payload == data
